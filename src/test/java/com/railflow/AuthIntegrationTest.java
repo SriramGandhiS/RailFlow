c7847094407adf51ed8eea4f2c2000cb460aaa5b
SriramGandhiS <iamramm8@gmail.com>
2026-06-21 18:14:07 +0530

    test: add register and login integration tests

---
+++ src/test/java/com/railflow/AuthIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.railflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AuthIntegrationTest {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
